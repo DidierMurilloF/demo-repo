5e949bddc2d0f7b7a8efd564c03b33c5c4a08504
Didier Murillo Florez <didier.murilloflorez@ndsu.edu>
2022-11-04 09:33:59 -0500

    update sum function

--- sum.R
+++ sum.R
@@ -2,5 +2,7 @@
 #' @param a is an integer 
 #' @param b is an integer
 sum_two <- function(a, b) {
+	if (missing(a)) stop("Error: a is missing")
+	if (missing(b)) stop("Error: b is missing")
   return(a + b)
 }
